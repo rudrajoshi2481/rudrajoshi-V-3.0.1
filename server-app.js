6a251ee9089a78798a7b8366858a64b0a41d58c2
rudrajoshi2481 <rudrajoshi2481@gmail.com>
2021-03-23 22:33:01 +0530

    final commit version 02

--- server-app.js
+++ server-app.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
+const path = require("path")
 const app = express()
 
-app.use(express.static('build'))
+app.use('/',express.static(path.join(__dirname, "build")))
+
+app.get('*', (req, res) => res.sendFile(path.resolve('', 'build', 'index.html')));
+
+// // Serve static files on server
+// app.use("/public", express.static(__dirname + "/../public"));
+
+// app.get("*", function(request, response) {
+//   response.sendFile(path.join(__dirname + "/../public/index.html"));
+// });
+
 
 
 app.listen(1000,() => {
